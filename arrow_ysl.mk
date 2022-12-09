70ce10e707c541d276d8d6af038caf14de0dfa8e
ChampionsGod <96783587+ChampionsGod@users.noreply.github.com>
2022-12-09 11:26:09 +0530

    ysl: Update Build fp from sunfish Dec'22 A13

--- arrow_ysl.mk
+++ arrow_ysl.mk
@@ -34,7 +34,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ysl-user 9 PKQ1.181203.001 V12.0.2.0.PEFMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/sunfish/sunfish:13/TP1A.221105.002/9080065:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:13/TQ1A.221205.011/9244662:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
